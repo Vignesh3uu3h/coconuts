545a462ce3ceb829c875744b7fd90eeb7e96abdc
Vignesh3uu3h <160583949+Vignesh3uu3h@users.noreply.github.com>
2026-03-30 11:12:09 +0530

    connect frontend to live backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://coconut-api-04l7.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
