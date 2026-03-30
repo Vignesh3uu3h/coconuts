333118880312fa3ed9f5e21fec2edcfc77ec16c7
Vignesh3uu3h <160583949+Vignesh3uu3h@users.noreply.github.com>
2026-03-30 11:27:10 +0530

    fix auth api paths

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://coconut-api-04l7.onrender.com',
+  baseURL: 'https://coconut-api-04l7.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
